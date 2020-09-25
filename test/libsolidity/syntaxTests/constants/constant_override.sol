3ef733c5be5dedcec3add9c2079aa244d0a9d34a
chriseth <chris@ethereum.org>
2020-09-25 12:21:55 +0200

    New tests.

---
+++ test/libsolidity/syntaxTests/constants/constant_override.sol
@@ -0,0 +1,3 @@
+uint constant override x;
+// ----
+// TypeError 4266: (0-24): Uninitialized "constant" variable.
---
+++ test/libsolidity/syntaxTests/constants/constant_with_visibility.sol
@@ -0,0 +1,3 @@
+uint public x = 7;
+// ----
+// DeclarationError 8342: (0-17): Only constant variables are allowed at file level.
